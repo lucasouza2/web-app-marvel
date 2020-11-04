aa1e92da4258609194e07848d6876db20089947d
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2020-11-04 20:48:40 -0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(function () {
-	const apikey = '&apikey=57f92f2acc1aa8c873560a8e5f1648f0';
-	var baseUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=Tony`;
+	const apikey = 'apikey=57f92f2acc1aa8c873560a8e5f1648f0';
+	var baseUrl = `http://gateway.marvel.com/v1/public/`;
+	var text = ''
+	var offset = 0;
 	$('#btn').click(function () {
+		getCharacter("Tony")
+	});
+
+	$('#scroll').click(function () {
+		get20Characters()
+	});
+
+	function getCharacter(name) {
 		$.ajax({
-			url: baseUrl+apikey,
+			url: `${baseUrl}characters?nameStartsWith=${name}&${apikey}`,
 			type: 'GET',
 			dataType: 'JSON',
 			success: function (result) {
@@ -13,5 +23,36 @@ $(function () {
 				console.log(`Error: ${error}`);
 			},
 		});
-	});
+	}
+
+	function get20Characters() {
+		$.ajax({
+			url: `${baseUrl}characters?offset=${offset}&${apikey}`,
+			type: 'GET',
+			dataType: 'JSON',
+			success: function (result) {
+				let results = result.data.results
+				for (let i = 0; i < results.length; i++) {
+					createArticle(results[i].name)
+					// text += `${results[i].name} <br>`
+				}
+				// $('#results').html(text)
+				offset += 20;
+			},
+			error: function (error) {
+				console.log(`Error: ${error}`);
+			},
+		});
+	}
+
+	function createArticle(name) {
+		var node;
+		var textnode;
+		node = document.createElement('article');
+		textnode = document.createTextNode(name);
+		node.setAttribute('id', name);
+		node.setAttribute('class', 'post');
+		node.appendChild(textnode);
+		document.getElementById('container').appendChild(node);
+	}
 });
