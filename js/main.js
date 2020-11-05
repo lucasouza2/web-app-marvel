b5a05b95662f959d17e1055d12e5082136b93d3e
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2020-11-05 00:57:57 -0300

    carregar 20 personagens ao chegar no fim da página

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 $(function () {
 	const apikey = 'apikey=57f92f2acc1aa8c873560a8e5f1648f0';
 	var baseUrl = `http://gateway.marvel.com/v1/public/`;
-	var text = '';
 	var offset = 0;
-	$('#btn').click(function () {
-		getCharacter('Tony');
+
+	$(document).ready(function () {
+		get20Characters();
 	});
 
-	$('#scroll').click(function () {
-		get20Characters();
+	$('#pesquisa').click(function () {
+		var name = $('#inputName').val();
+		getCharacter(name);
 	});
 
 	function getCharacter(name) {
@@ -17,13 +18,25 @@ $(function () {
 			type: 'GET',
 			dataType: 'JSON',
 			success: function (result) {
-				$('#results').html(result.data.results[0].name);
+				var results = result.data.results;
+				addLI(results[0].name, results[0].thumbnail);
 			},
 			error: function (error) {
 				console.log(`Error: ${error}`);
 			},
 		});
 	}
+
+	var win = $(window);
+
+	// Each time the user scrolls
+	win.scroll(function () {
+		// End of the document reached?
+		if ($(document).height() - win.height() == win.scrollTop()) {
+			// TODO: Adcionar loading gif
+			get20Characters()
+		}
+	});
 
 	function get20Characters() {
 		$.ajax({
@@ -33,10 +46,8 @@ $(function () {
 			success: function (result) {
 				let results = result.data.results;
 				for (let i = 0; i < results.length; i++) {
-					createArticle(results[i].name);
-					// text += `${results[i].name} <br>`
+					addLI(results[i].name, results[i].thumbnail);
 				}
-				// $('#results').html(text)
 				offset += 20;
 			},
 			error: function (error) {
@@ -45,14 +56,11 @@ $(function () {
 		});
 	}
 
-	function createArticle(name) {
-		var node;
-		var textnode;
-		node = document.createElement('article');
-		textnode = document.createTextNode(name);
-		node.setAttribute('id', name);
-		node.setAttribute('class', 'post');
-		node.appendChild(textnode);
-		document.getElementById('container').appendChild(node);
+	function addLI(name, backgroundimage) {
+		$('#personagens').append(
+			$(
+				`<li style='background-image: url(${backgroundimage.path}/standard_fantastic.${backgroundimage.extension})'></li>`
+			).append($('<h2></h2>').text(name))
+		);
 	}
 });
